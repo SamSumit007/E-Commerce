08c57c7f399ffc4354b6c0c0b45e4e700e69bbac
Sumit Thakur <97454993+SamSumit007@users.noreply.github.com>
2022-07-21 19:23:49 +0530

    brand and product page updated

--- frontend/src/components/UserDetails/Address.jsx
+++ frontend/src/components/UserDetails/Address.jsx
@@ -1,11 +1,63 @@
 import React from "react";
-
+import { useState, useEffect, useContext } from "react";
+import { UserContext } from "../../context/UserContext";
+import { AddressContext } from "../../context/AddressContext";
+import { nanoid } from "nanoid";
+import { Link } from "react-router-dom";
 export const Address = () => {
   const [data, setdata] = useState();
+  const { user_id } = useContext(UserContext);
+  const { handleAddressIndex } = useContext(AddressContext);
+  function handleEditAddress(value) {
+    handleAddressIndex(value);
+  }
+  async function getData() {
+    await fetch(`http://localhost:5500/users/${user_id}/addresses`).then(
+      async (d) => {
+        let responseData = await d.json();
+        setdata(responseData.user);
+      }
+    );
+  }
+
+  useEffect(() => {
+    if (user_id !== "") {
+      getData();
+    }
+  }, [user_id]);
+
   
   return (
     <div to="users/:id/address">
-    
+    <Link to="create">add an Address</Link>
+      {data ? (
+        <div>
+          {data.address.map((el, index) => {
+            return (
+              <div key={nanoid(5)}>
+                <h2>Address: {index + 1}</h2>
+                <div>Line 1 {el.line_1}</div>
+                <div>Line 2 {el.line_2}</div>
+                <div>City{el.city} </div>
+                <div>postalCode{el.postalCode}</div>
+                <div>state{el.state}</div>
+                <Link
+                  to={`${index}/edit`}
+                  onClick={(e) => {
+                    handleEditAddress(index);
+                  }}
+                >
+                  Edit Address
+                </Link>
+              </div>
+            );
+          })}
+
+          {/* <Address add={userdetails._id} /> */}
+        </div>
+      ) : (
+        <div>getting the data from the server</div>
+      )}
     </div>
   );
 };
--- frontend/src/components/UserDetails/UserDetails.jsx
+++ frontend/src/components/UserDetails/UserDetails.jsx
@@ -1,10 +1,46 @@
 import React from "react";
+import { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "../../context/UserContext";
+import { createBrowserHistory } from "history";
 
 export const UserDetails = () => {
- 
+  const history = createBrowserHistory();
+  let realId = history.location.pathname.split("/")[2];
+
+  const [data, setdata] = useState();
+  const { user_id } = useContext(UserContext);
+
+  async function getData() {
+    await fetch(`http://localhost:5500/users/${realId}`).then(async (d) => {
+      let responseData = await d.json();
+      // console.log(user_id, "userid");
+
+      setdata(responseData.user);
+    });
+  }
+
+  useEffect(() => {
+    if (user_id !== "") {
+      getData();
+    }
+  }, [user_id]);
   return (
     <div to="users/:id">
-      
+       <h2>User Details</h2>
+      {data ? (
+        <div>
+          <div>NAME: {data.name}</div>
+          <div>MOBILE NO. {data.mobileNumber}</div>
+          <Link to="edit">Edit user details</Link>
+          <h4>addresses:</h4>
+
+          {/* <Address add={userdetails._id} /> */}
+          <Link to="addresses">See Addresses</Link>
+        </div>
+      ) : (
+        <div>getting the data from the server</div>
+      )}
     </div>
   );
 };
